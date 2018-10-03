a69f8c02b67060370ba3ce9bd83b9a41d01db71f
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-03 23:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Add your functions and code here
 
 var kittens = ["Milo", "Otis", "Garfield"];
-var newKitten = "Ralph"
 
 function destructivelyAppendKitten(kittens, newKitten) {
   kittens.push(newKitten);
